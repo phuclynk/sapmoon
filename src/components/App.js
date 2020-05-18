1e9939bda7cb8575dd01d48024936c6cdfe0c54d
phuc tran <phuc.tran@fiisoft.com>
2020-05-18 16:13:22 +0700

    reconfig-eslint

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 const App = () => {
+    const phuc = 'oke';
+
     return (
         <div>
             <h1>My React App</h1>
         </div>
     );
-}
+};
 
 export default App;

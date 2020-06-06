e659a3eb7d2dd56c18fbfcec7b5ae3fee10133d4
Phuc Tran <>
2020-06-06 14:10:42 +0700

    project struct

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
-import './style/global.scss';
+import './shared/style/global.scss';
 
 ReactDOM.render(<App/>, document.getElementById('app'));

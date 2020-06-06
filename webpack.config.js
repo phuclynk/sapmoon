01aecbdf1b8225e2001143eeb8de18939c6042f4
Phuc Tran <>
2020-06-06 15:22:49 +0700

    config wepack env

--- webpack.config.js
+++ webpack.config.js
@@ -41,7 +41,7 @@ module.exports = {
             template: './src/index.html'
         }),
         new Dotenv({
-            path: './.env', // load this now instead of the ones in '.env'
+            // path: './.env', // load this now instead of the ones in '.env's
             safe: true, // load '.env.example' to verify the '.env' variables are all set. Can also be a string to a different file.
             allowEmptyValues: true, // allow empty variables (e.g. `FOO=`) (treat it as empty string, rather than missing)
             systemvars: true, // load all the predefined 'process.env' variables which will trump anything local per dotenv specs.
